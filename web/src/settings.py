942b282df9ef2b0089d82b3f1f0a771eb06a6c87
Olim2508 <rahmatovolim3@gmail.com>
2022-11-25 11:02:20 +0500

    Prod: added media files handling on prod

--- web/src/settings.py
+++ web/src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'main'
 ]
 
 MIDDLEWARE = [
@@ -131,6 +132,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
 
 
 # Default primary key field type
